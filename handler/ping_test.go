4b35252d6f4e52519561d3fd7366217d76bee881
Fattesingh Rane <41385129+ranefattesingh@users.noreply.github.com>
2022-08-14 15:46:24 -0400

    Update ping_test.go

--- handler/ping_test.go
+++ handler/ping_test.go
@@ -10,12 +10,13 @@ import (
 
 func TestPingHandler(t *testing.T) {
 	t.Parallel()
-	
+
 	req := httptest.NewRequest(http.MethodGet, "/ping", nil)
 	writer := httptest.NewRecorder()
 	handler.Ping(writer, req)
 
 	if result := writer.Result(); result.StatusCode != http.StatusOK {
 		t.Errorf("Expected status = %v but got status = %v", http.StatusOK, result.StatusCode)
+		defer result.Body.Close()
 	}
 }
