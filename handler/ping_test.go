ae5773468a0209b9c5c7af5bd53599674d6938a9
Fattesingh Rane <41385129+ranefattesingh@users.noreply.github.com>
2022-08-14 15:32:40 -0400

    Update ping_test.go

--- handler/ping_test.go
+++ handler/ping_test.go
@@ -10,6 +10,7 @@ import (
 
 func TestPingHandler(t *testing.T) {
 	t.Parallel()
+	
 	req := httptest.NewRequest(http.MethodGet, "/ping", nil)
 	writer := httptest.NewRecorder()
 	handler.Ping(writer, req)
